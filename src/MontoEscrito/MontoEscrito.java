092595b2bbeb1f9253e693ada3320694de330816
Alejandra Rivera <alejarivrin@hotmail.com>
2018-08-18 20:35:47 -0500

    switch validando unidades,decenas, centenas y unidades de mil.

--- src/MontoEscrito/MontoEscrito.java
+++ src/MontoEscrito/MontoEscrito.java
@@ -2,4 +2,185 @@ package MontoEscrito;
 
 public class MontoEscrito {
 
+	public String Letras(String numero) {
+
+		String letras = "";
+		
+		switch (numero.substring(0, 1)) {
+
+		case "1":
+			letras = "MIL";
+			break;
+			
+		case "2":
+			letras = "DOS MIL";
+			break;
+		
+		case "3":
+			letras = "TRES MIL";
+			break;
+			
+		case "4":
+			letras = "CUATRO MIL";
+			break;
+			
+		case "5":
+			letras = "CINCO MIL";
+			break;
+			
+		case "6":
+			letras = "SEIS MIL";
+			break;
+			
+		case "7":
+			letras = "SIETE MIL";
+			break;
+			
+		case "8":
+			letras = "OCHO MIL";
+			break;
+			
+		case "9":
+			letras = "NUEVE MIL";
+			break;
+			
+		default:
+			letras = "";
+			break;
+		}
+		
+		switch (numero.substring(1, 2)) {
+
+		case "1":
+			letras = "CIEN";
+			break;
+			
+		case "2":
+			letras = "DOSCIENTOS";
+			break;
+		
+		case "3":
+			letras = "TRESCIENTOS";
+			break;
+			
+		case "4":
+			letras = "CUATROCIENTOS";
+			break;
+			
+		case "5":
+			letras = "QUINIENTOS";
+			break;
+			
+		case "6":
+			letras = "SEISCIENTOS";
+			break;
+			
+		case "7":
+			letras = "SIETECIENTOS";
+			break;
+			
+		case "8":
+			letras = "OCHOCIENTOS";
+			break;
+			
+		case "9":
+			letras = "NOVECIENTOS";
+			break;
+			
+		default:
+			letras = "";
+			break;
+		}
+		
+		switch (numero.substring(2, 3)) {
+
+		case "1":
+			letras = "DIEZ";
+			break;
+			
+		case "2":
+			letras = "VEINTE";
+			break;
+		
+		case "3":
+			letras = "TREINTA";
+			break;
+			
+		case "4":
+			letras = "CUARENTA";
+			break;
+			
+		case "5":
+			letras = "CINCUENTA";
+			break;
+			
+		case "6":
+			letras = "SESENTA";
+			break;
+			
+		case "7":
+			letras = "SETENTA";
+			break;
+			
+		case "8":
+			letras = "OCHENTA";
+			break;
+			
+		case "9":
+			letras = "NOVENTA";
+			break;
+			
+		default:
+			letras = "";
+			break;
+		}
+		
+		switch (numero.substring(3, 43)) {
+
+		case "1":
+			letras = "UNO";
+			break;
+			
+		case "2":
+			letras = "DOS";
+			break;
+		
+		case "3":
+			letras = "TRES";
+			break;
+			
+		case "4":
+			letras = "CUATRO";
+			break;
+			
+		case "5":
+			letras = "CINCO";
+			break;
+			
+		case "6":
+			letras = "SEIS";
+			break;
+			
+		case "7":
+			letras = "SIETE";
+			break;
+			
+		case "8":
+			letras = "OCHO";
+			break;
+			
+		case "9":
+			letras = "NUEVE";
+			break;
+			
+		default:
+			letras = "";
+			break;
+		}
+
+		return letras;
+
+	}
+	
+	
 }
--- src/Test/MontoEscritoTest.java
+++ src/Test/MontoEscritoTest.java
@@ -1,14 +1,29 @@
 package Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import MontoEscrito.MontoEscrito;
 import org.junit.jupiter.api.Test;
 
 class MontoEscritoTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCuatroDigitos() {
+		MontoEscrito ValorLetras = new MontoEscrito();
+		String letrasesperadas = "NUEVE MIL";
+		String letrasobtenidas = ValorLetras.Letras("9000");
+		System.out.print(letrasobtenidas);
+		assertEquals(letrasesperadas, letrasobtenidas);
+
+	}
+	
+	@Test
+	void testTresDigitos() {
+		MontoEscrito ValorLetras = new MontoEscrito();
+		String letrasesperadas = "QUINIENTOS";
+		String letrasobtenidas = ValorLetras.Letras("0511");
+		System.out.print(letrasobtenidas);
+		assertEquals(letrasesperadas, letrasobtenidas);
+
 	}
 
 }
